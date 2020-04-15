eccde7e9cf7ecb4c4efca46bcadfe14bb3f04914
Tanguy Jouvin <42809139+tanguyJouvin@users.noreply.github.com>
2020-04-15 19:00:05 +0200

    add
    add

--- routes/tags.js
+++ routes/tags.js
@@ -1,12 +1,24 @@
 const express = require('express');
-const tags = require('../data/tags');
-
 const router = express.Router({ mergeParams: true });
 
+const tags = require('../data/tags');
+const fakePosts = require('../data/posts');
+//get /api/tags
 router.get('/', (req, res) => {
   res.json(tags);
 })
 
+router.get('/:tagId/posts', (req, res) =>{
+  const tagId = Number(req.params.tagId);
+  const foundTag = tags.find(tag => tag.id === tagId);
+  if (!foundTag) {
+    return res.status(404).json({
+      error: 'Post not found',
+    });
+  }
+  const goodPost = fakePosts.filter(el => el['tag_ids'] === tagId);
+  res.send(goodPost);
+})
 // Get a list of comments
 // router.get('/', (req, res) => {
 //   const postId = Number(req.params.postId);
